e13798529f978d5a0a55ba50220aee3c314ad815
heyang <1804042148@qq.com>
2022-01-23 00:22:19 +0800

    第一次上传文件

---
+++ src/main/java/com/example/alibabagateway/AlibabaGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.alibabagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlibabaGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlibabaGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/alibabagateway/AlibabaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alibabagateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlibabaGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
